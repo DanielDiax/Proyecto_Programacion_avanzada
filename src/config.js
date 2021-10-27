ea2c7cf8df3c8f00ca2ae16d8f6733a121b5f354
Cristian Henao <analista3@mic.com.co>
2021-10-27 13:26:05 -0500

    Se agrega Back a la nube en heroku y se traslada la DB amongo atlas

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@ import { config } from "dotenv";
 config();
 
 export default {
-  mongodbURL: process.env.MONGODB_URI || "mongodb://localhost/TestDB",
+  mongodbURL: process.env.MONGODB_URI || "mongodb+srv://jdanieldiax:Hanna.2020_01_01@cluster0.lfvma.mongodb.net/TestDB",
 };
+
+
+//mongodb://localhost/TestDB //Local
